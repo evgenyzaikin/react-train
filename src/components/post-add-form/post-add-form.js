1a12bd669be742b27f5fabb22ad03d9c3f5d97cd
evgenyzaikin <evgenyzaikin96@gmail.com>
2022-05-19 17:26:51 +0300

    init

---
+++ src/components/post-add-form/post-add-form.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./post-add-form.css";
+
+const PostAddForm = () => {
+  return (
+    <form className="bottom-panel d-flex">
+      <input
+        type="text"
+        placeholder="Напиши твит сюда"
+        className="form-control new-post-label"
+      />
+      <button type="submit" className="btn btn-outline-secondary">
+        Добавить
+      </button>
+    </form>
+  );
+};
+
+export default PostAddForm;
