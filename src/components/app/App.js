f084216127a0eb60135a5e4776b2e31265511b2d
evgenyzaikin <evgenyzaikin96@gmail.com>
2022-05-20 13:31:59 +0300

    post data from add

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import AppHeader from '../app-header';
-import SearchPanel from '../search-panel';
-import PostStatusFilter from '../post-status-filter';
-import PostList from '../post-list';
-import PostAddForm from '../post-add-form/post-add-form';
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import PostStatusFilter from "../post-status-filter";
+import PostList from "../post-list";
+import PostAddForm from "../post-add-form/post-add-form";
 
 function App() {
+  const data = [
+    { label: "Hi, I'm a first tweet!", isImportant: true },
+    { label: "I'm a second tweet!", isImportant: false },
+    {
+      label: "Yep, this is third tweet, how did you guess?",
+      isImportant: false,
+    },
+  ];
+
   return (
-    <div className='app'>
-      <AppHeader/>
-      <div className='search-panel d-flex'>
-        <SearchPanel/>
-        <PostStatusFilter/>
+    <div className="app">
+      <AppHeader />
+      <div className="search-panel d-flex">
+        <SearchPanel />
+        <PostStatusFilter />
       </div>
-      <PostList/>
-      <PostAddForm/>
+      <PostList posts={data}/>
+      <PostAddForm />
     </div>
   );
 }
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./post-list.css";
 
-import PostListItem from '../post-list-item';
+import PostListItem from "../post-list-item";
 
-const PostList = () => {
-    return(
-        <ul className="app-list list-group">
-            <PostListItem label="Hi, I'm a first tweet!" isImportant/>
-            <PostListItem label="I'm a second tweet!"/>
-            <PostListItem label="Yep, this is third tweet, how did you guess?"/>
-        </ul>
-    )
+const PostList = ({ posts }) => {
+  const elememts = posts.map((item) => {
+    return <PostListItem label={item.label} isImportant={item.isImportant} />;
+  });
+  return <ul className="app-list list-group">{elememts}</ul>;
 };
 
 export default PostList;
